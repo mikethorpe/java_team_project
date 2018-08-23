cb510166b0be7bf5048b62d5116c8febb5be57a1
mikethorpe <michael.a.thorpe@gmail.com>
2018-08-23 22:51:55 +0100

    Adding DBHelper with generic Hibernate save / update method. Adding Hibernate config.

---
+++ javablog/src/main/java/db/DBHelper.java
@@ -0,0 +1,30 @@
+package db;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class DBHelper {
+
+	private static Session session;
+	private static Transaction transaction;
+
+	public static void save(Object object){
+		session = HibernateUtil.getSessionFactory().openSession();
+
+		try {
+			transaction = session.beginTransaction();
+			session.saveOrUpdate(object);
+			transaction.commit();
+		}
+		catch (HibernateException ex){
+			transaction.rollback();;
+			ex.printStackTrace();
+		}
+		finally {
+			session.close();
+		}
+	}
+
+
+}
