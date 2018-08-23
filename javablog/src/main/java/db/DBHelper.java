fc57ad064216425cbac86fddf306a7e0f2bed388
mikethorpe <michael.a.thorpe@gmail.com>
2018-08-23 23:10:20 +0100

    Adding generic Hibernate delete method in DBHelper

--- javablog/src/main/java/db/DBHelper.java
+++ javablog/src/main/java/db/DBHelper.java
@@ -3,6 +3,7 @@ package db;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class DBHelper {
 
@@ -19,6 +20,23 @@ public class DBHelper {
 		}
 		catch (HibernateException ex){
 			transaction.rollback();;
+			ex.printStackTrace();
+		}
+		finally {
+			session.close();
+		}
+	}
+
+	public static void delete(Object object){
+		session = HibernateUtil.getSessionFactory().openSession();
+
+		try {
+			transaction = session.beginTransaction();
+			session.delete(object);
+			transaction.commit();
+		}
+		catch (HibernateException ex){
+			transaction.rollback();
 			ex.printStackTrace();
 		}
 		finally {
