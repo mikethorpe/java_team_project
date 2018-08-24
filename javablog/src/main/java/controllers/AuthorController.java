66b522fba3292f53906e10e16b666621dda12416
Molly Drumm <m.drumm89@googlemail.com>
2018-08-24 13:35:02 +0100

    adding get request for author

--- javablog/src/main/java/controllers/AuthorController.java
+++ javablog/src/main/java/controllers/AuthorController.java
@@ -1,9 +1,12 @@
 package controllers;
 
+import db.DBHelper;
+import models.Author;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
@@ -21,6 +24,15 @@ public class AuthorController {
 		get("/authors/new", (req, res) ->{
 			Map<String, Object> model = new HashMap<>();
 			model.put("template", "templates/authors/new.vtl");
+			return new ModelAndView(model, "templates/layout.vtl");
+		}, velocityTemplateEngine
+		);
+
+		get("/authors", (req, res) ->{
+			Map<String, Object> model = new HashMap<>();
+			List<Author> authors = DBHelper.findAll(Author.class);
+			model.put("template", "templates/authors/index.vtl");
+			model.put("authors", authors);
 			return new ModelAndView(model, "templates/layout.vtl");
 		}, velocityTemplateEngine
 		);
