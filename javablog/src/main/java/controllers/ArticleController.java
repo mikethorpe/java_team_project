432e678aa739f1d450af1460f9fede292d19f522
Molly Drumm <m.drumm89@googlemail.com>
2018-08-24 16:07:08 +0100

    adding post method to article

--- javablog/src/main/java/controllers/ArticleController.java
+++ javablog/src/main/java/controllers/ArticleController.java
@@ -1,16 +1,27 @@
 package controllers;
 
+import db.DBHelper;
+import models.Article;
+import models.Author;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class ArticleController {
 
+
+    public ArticleController(){
+        setupEndPoints();
+    }
+
     private static void setupEndPoints() {
+
 
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
 
@@ -20,5 +31,23 @@ public class ArticleController {
                     return new ModelAndView(model, "templates/layout.vtl");
                 }, velocityTemplateEngine
         );
+
+        get("/articles", (req, res) ->{
+                    Map<String, Object> model = new HashMap<>();
+                    List<Article> articles = DBHelper.findAll(Article.class);
+                    model.put("template", "templates/articles/index.vtl");
+                    model.put("articles", articles);
+                    return new ModelAndView(model, "templates/layout.vtl");
+                }, velocityTemplateEngine
+        );
+
+        post("/articles", (req, res) -> {
+            String title = req.queryParams("title");
+            String textContent = req.queryParams("text_content");
+            Article article = new Article(title, textContent);
+            DBHelper.save(article);
+            res.redirect("/articles");
+            return null;
+        }, velocityTemplateEngine);
     }
 }
