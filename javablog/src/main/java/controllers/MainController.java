bf86945797dcfbcfc241443959b8663e754e19b3
Molly Drumm <m.drumm89@googlemail.com>
2018-08-24 14:23:49 +0100

    adding view details

--- javablog/src/main/java/controllers/MainController.java
+++ javablog/src/main/java/controllers/MainController.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import db.DBHelper;
+import models.Author;
+
 public class MainController {
 
 	public static void main(String[] args) {
 
+		Author author = new Author("Molly");
+		DBHelper.save(author);
 		AuthorController authorController = new AuthorController();
 	}
 }
