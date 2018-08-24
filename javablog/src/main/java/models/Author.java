aa41e2ff39bb97cc436b1ddd9e54a548ecdc73d3
Molly Drumm <m.drumm89@googlemail.com>
2018-08-24 10:53:17 +0100

    adding anotations to author

--- javablog/src/main/java/models/Author.java
+++ javablog/src/main/java/models/Author.java
@@ -1,14 +1,24 @@
 package models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authors")
 public class Author {
 
     private int id;
     private String name;
 
+    public Author(){};
+
     public Author(String name) {
         this.name = name;
     }
 
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     public int getId() {
         return id;
     }
@@ -17,6 +27,7 @@ public class Author {
         this.id = id;
     }
 
+    @Column(name = "name")
     public String getName() {
         return name;
     }
