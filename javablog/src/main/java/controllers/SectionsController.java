875ea0d945bec0fc79467142c8159b387adbaeb9
mikethorpe <michael.a.thorpe@gmail.com>
2018-08-24 17:51:37 +0100

    Adding post request to create new Section

--- javablog/src/main/java/controllers/SectionsController.java
+++ javablog/src/main/java/controllers/SectionsController.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import db.DBHelper;
+import models.Section;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.HashMap;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class SectionsController {
 
 	public SectionsController() {
@@ -19,6 +23,14 @@ public class SectionsController {
 			model.put("template", "templates/sections/new.vtl");
 
 			return new ModelAndView(model, "templates/layout.vtl");
+		}, new VelocityTemplateEngine());
+
+		post("/sections", (req, res) -> {
+			String title = req.queryParams("title");
+			Section section = new Section(title);
+			DBHelper.save(section);
+			res.redirect("/sections");
+			return null;
 		}, new VelocityTemplateEngine());
 	}
 }
