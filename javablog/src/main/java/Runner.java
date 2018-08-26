f71146f285a23f3c1930a3aab1d73607af1c8b79
mikethorpe <michael.a.thorpe@gmail.com>
2018-08-26 13:13:14 +0100

    Adding query to find author's list of articles

--- javablog/src/main/java/Runner.java
+++ javablog/src/main/java/Runner.java
@@ -13,11 +13,19 @@ public class Runner {
 
         Author author1 = new Author("Mike");
         DBHelper.save(author1);
+
         Author author2 = new Author("Molly");
         DBHelper.save(author2);
 
-        Article article = new Article("Mike and molly code", "Amazing coding story", author1);
-        DBHelper.save(article);
+        Article article1 = new Article("Article1 title", "Article 1 content", author1);
+        DBHelper.save(article1);
+
+		Article article2 = new Article("Article2 title", "Article 2 content", author2);
+		DBHelper.save(article2);
+
+		Article article3 = new Article("Article3 title", "Article 3 content", author2);
+		DBHelper.save(article3);
+
 
 		Section section = new Section("Technology");
 		DBHelper.save(section);
@@ -25,9 +33,11 @@ public class Runner {
 		List<Section> sections = new ArrayList<>();
 		sections.add(section);
 
-		DBArticle.updateArticlesSections(article, sections);
+		DBArticle.updateArticlesSections(article1, sections);
 
 		Article foundArticle = DBHelper.findById(Article.class, 1);
 		Section foundSection = DBHelper.findById(Section.class, 1);
+
+		List<Article> mollysArticles = DBArticle.findAllAuthorsArticles(author2);
     }
 }
--- javablog/src/main/java/db/DBArticle.java
+++ javablog/src/main/java/db/DBArticle.java
@@ -1,11 +1,18 @@
 package db;
 
 import models.Article;
+import models.Author;
 import models.Section;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 import java.util.List;
 
 public class DBArticle {
+	private static Session session;
 
 	public static void updateArticlesSections(Article article, List<Section> sections){
 		article.clearArticleSections();
@@ -15,4 +22,25 @@ public class DBArticle {
 		}
 		DBHelper.save(article);
 	}
+
+	public static List<Article> findAllAuthorsArticles(Author author){
+		session = HibernateUtil.getSessionFactory().openSession();
+		List<Article> results = null;
+
+		try {
+			Criteria cr = session.createCriteria(Article.class);
+			cr.add(Restrictions.eq("author", author));
+			cr.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+			cr.addOrder(Order.asc("id"));
+			results = cr.list();
+		}
+		catch (HibernateException ex){
+			ex.printStackTrace();
+		}
+		finally {
+			session.close();
+		}
+		return results;
+	}
+
 }
