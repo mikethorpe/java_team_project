4a43894f7a238db2058dc21fd524ec8bd26a5ccf
Molly Drumm <m.drumm89@googlemail.com>
2018-08-29 21:38:41 +0100

    seeding articles 2 and 3

--- javablog/src/main/java/controllers/MainController.java
+++ javablog/src/main/java/controllers/MainController.java
@@ -55,8 +55,14 @@ public class MainController {
 		Section section2 = new Section("Art");
 		DBHelper.save(section2);
 
-		Section section3 = new Section("Environment");
-		DBHelper.save(section1);
+		Section section3 = new Section("Sport");
+		DBHelper.save(section3);
+
+        Section section4 = new Section("Cooking");
+        DBHelper.save(section4);
+
+        Section section5 = new Section("Environment");
+        DBHelper.save(section5);
 
 
 		List<Section> home = new ArrayList<>();
@@ -64,6 +70,15 @@ public class MainController {
 
 		List<Section> artSections = new ArrayList<>();
 		artSections.add(section2);
+
+        List<Section> sportSections = new ArrayList<>();
+        sportSections.add(section3);
+
+        List<Section> cookingSections = new ArrayList<>();
+        artSections.add(section4);
+
+        List<Section> environmentSections = new ArrayList<>();
+        artSections.add(section5);
 
 		// Set up authors
 		Author author1 = new Author("Mike");
@@ -117,23 +132,94 @@ public class MainController {
 		article2.updateArticleDate();
 		DBHelper.save(article2);
 
-		Article article3 = new Article("Article3 title", "Article 3 content", author2);
+		Article article3 = new Article("\"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, " +
+                "consectetur, adipisci velit...\"\n" +
+                "\"There is no one who loves pain itself, who seeks after it and wants to have it, simply because " +
+                "it is pain...\"", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ornare" +
+                " mauris a nunc vestibulum, eu cursus nisi luctus. Suspendisse mattis ornare velit, sit amet vulputate" +
+                " justo finibus sit amet. Suspendisse malesuada et lectus quis sollicitudin. Cras et mattis ante. Cras" +
+                " viverra hendrerit consequat. Duis ornare, nunc eget consequat pellentesque, enim eros hendrerit magna," +
+                " laoreet eleifend purus mi tempor purus. Duis eget enim sit amet diam pretium porta vitae ac metus." +
+                " Mauris ullamcorper est sed bibendum facilisis. Aliquam blandit lobortis odio at ultrices. Vestibulum" +
+                " ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur lorem tortor, " +
+                "maximus et interdum sed, pharetra ut metus. Cras magna velit, cursus quis elit nec, ultricies mollis " +
+                "leo. Proin nec mi velit. Maecenas eu enim nulla. Fusce laoreet pretium turpis vitae iaculis. Aenean " +
+                "mi velit, dignissim ut enim in, consequat blandit neque.\n" +
+                "\n" +
+                "Nam tristique purus sit amet bibendum ullamcorper. Etiam urna risus, porttitor at consectetur " +
+                "vitae, sagittis at nibh. Proin eget pulvinar justo. Curabitur a orci eu turpis vulputate viverra." +
+                " Nulla auctor volutpat risus, a condimentum dui dapibus et. Integer consequat quis ante et auctor." +
+                " Integer lectus dui, malesuada posuere nisl a, tristique varius eros. In non consequat odio. In " +
+                "iaculis pharetra orci vitae pellentesque. Curabitur nisl dui, pharetra at nisi vitae, mollis dapibus " +
+                "ante. Proin ornare diam in massa tristique elementum. Ut id lorem augue. Nam at semper tortor. Cras" +
+                " sed maximus ex. Ut eget turpis sed elit aliquet placerat.\n" +
+                "\n" +
+                "Suspendisse scelerisque fermentum diam ut feugiat. Nunc at tristique libero. Phasellus vestibulum" +
+                " nulla nulla, ut vulputate felis iaculis sodales. Curabitur porttitor tristique ex, cursus tempor" +
+                " lectus interdum scelerisque. Vestibulum vitae risus libero. Duis ac leo id tortor cursus molestie." +
+                " Nullam ac nisi varius ligula congue convallis vel auctor ante. Aliquam consequat imperdiet " +
+                "rhoncus.\n" +
+                "\n" +
+                "Mauris quis metus ipsum. Duis eget massa lectus. Interdum et malesuada fames ac ante ipsum primis" +
+                " in faucibus. Vivamus et libero sed diam dapibus fermentum. Aliquam risus orci, tempus vitae " +
+                "condimentum at, mattis eu libero. Aliquam elit risus, condimentum et ipsum nec, rhoncus ullamcorper " +
+                "sem. Donec et magna non erat pharetra pretium id a arcu. Duis consectetur lectus quis mauris " +
+                "tincidunt, nec volutpat leo ullamcorper. Aenean diam massa, commodo ac hendrerit sed, pulvinar" +
+                " venenatis velit. Donec id est nec ipsum pulvinar placerat. Integer congue iaculis ante, at " +
+                "mollis mauris pellentesque vel. Nam scelerisque placerat orci sit amet fringilla. Proin tincidunt " +
+                "bibendum venenatis.\n" +
+                "\n" +
+                "Mauris quam urna, sagittis sed vehicula eu, eleifend commodo nibh. Vestibulum maximus consectetur" +
+                " turpis. Fusce pellentesque mi eget neque semper bibendum. Etiam et diam leo. Nullam viverra, sapien " +
+                "pretium malesuada bibendum, lorem sapien bibendum velit, non dapibus enim diam ut nisi. Nullam nec" +
+                " pharetra arcu. In vel vehicula felis. Vestibulum ante ipsum primis in faucibus orci luctus et " +
+                "ultrices posuere cubilia Curae; Sed dui lacus, dictum id auctor ac, tristique id tortor. Aliquam" +
+                " sodales imperdiet semper. Curabitur in auctor leo, vitae aliquam turpis.", author2);
 		article3.addView();
+		article3.setImageLink("https://154013-475334-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads" +
+                "/2018/06/cookingmatters_1080x1200-1024x640.jpg");
 		article3.updateArticleDate();
 		DBHelper.save(article3);
 
-		Article article1 = new Article("Article1 title", "Article 1 content", author1);
+
+        Article article1 = new Article("Neque porro quisquam est qui dolorem ipsum quia dolor sit amet," +
+                " consectetur, adipisci velit...", "Suspendisse posuere nunc eu massa " +
+                "sodales faucibus. Vivamus placerat, justo vitae ultricies sodales, felis eros facilisis nisl, " +
+                "at porta leo turpis ac massa. Suspendisse ut finibus purus. In dignissim lectus ac ornare feugiat. " +
+                "Nam semper mauris at quam egestas euismod. Morbi sodales dolor sed orci feugiat sollicitudin. Vivamus " +
+                "lacinia mi ligula, vitae sagittis tortor elementum et. Maecenas consectetur turpis nec porta " +
+                "condimentum. Vestibulum non nisi dolor. Mauris auctor magna id tincidunt varius. Mauris a " +
+                "sollicitudin arcu. Curabitur rutrum, turpis ut interdum maximus, mi dui scelerisque elit, id" +
+                " tincidunt dolor dui sollicitudin dui. Pellentesque quis convallis orci.\n" +
+                "\n" +
+                "Nullam at turpis in leo consequat sodales nec a est. Vestibulum nisl nunc, scelerisque feugiat " +
+                "tempor vitae, dapibus ut mauris. Curabitur ut tristique justo. Vestibulum ultrices interdum " +
+                "ligula, in efficitur sem blandit ut. Mauris vel metus rutrum, tincidunt est at, efficitur sapien. " +
+                "Proin sagittis semper congue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit " +
+                "amet varius magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per " +
+                "inceptos himenaeos. Aenean dignissim laoreet congue.\n" +
+                "\n" +
+                "Pellentesque sit amet magna quis lectus iaculis sagittis scelerisque quis purus. Mauris aliquam" +
+                " lectus mauris, at eleifend sapien blandit rutrum. Fusce lectus libero, facilisis a tortor non, " +
+                "suscipit molestie neque. Nulla egestas non mauris lobortis dictum. Aliquam eu pharetra orci. Mauris " +
+                "vulputate non libero ac imperdiet. Cras imperdiet nulla in dolor sagittis, vel egestas tortor " +
+                "scelerisque. Maecenas auctor neque blandit dui elementum cursus. Duis et iaculis mi.", author1);
 		article1.updateArticleDate();
 		article1.addView();
 		article1.addView();
 		article1.addView();
+        article1.setImageLink("https://www.incimages.com/uploaded_files/image/970x450/getty_517129164_279516.jpg");
 		DBHelper.save(article1);
 
+        DBArticle.updateArticlesSections(article2, home);
+        DBArticle.updateArticlesSections(article3, cookingSections);
+        DBArticle.updateArticlesSections(article1, environmentSections);
 
-		DBArticle.updateArticlesSections(article1, home);
-		DBArticle.updateArticlesSections(article2, home);
-		DBArticle.updateArticlesSections(article3, artSections);
-	}
+
+
+
+
+    }
 
 }
 
